840e79a51f5732ecbbc9b5889f03b67285879c3d
TeamMeng <862915265@qq.com>
2024-09-04 20:12:20 +0800

    feature: support atomic map

--- examples/ametrics.rs
+++ examples/ametrics.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use concurrency::Metrics;
+use concurrency::AmapMetrics;
 use rand::{thread_rng, Rng};
 use std::{thread, time::Duration};
 
@@ -7,7 +7,14 @@ const N: usize = 2;
 const M: usize = 4;
 
 fn main() -> Result<()> {
-    let metrics = Metrics::new();
+    let metrics = AmapMetrics::new(&[
+        "call.thread.worker.0",
+        "call.thread.worker.1",
+        "req.page.1",
+        "req.page.2",
+        "req.page.3",
+        "req.page.4",
+    ]);
 
     for idx in 0..N {
         task_work(idx, metrics.clone())?; // Arc::clone
@@ -23,7 +30,7 @@ fn main() -> Result<()> {
     }
 }
 
-fn task_work(idx: usize, metrics: Metrics) -> Result<()> {
+fn task_work(idx: usize, metrics: AmapMetrics) -> Result<()> {
     thread::spawn(move || {
         let mut rng = thread_rng();
         thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
@@ -33,7 +40,7 @@ fn task_work(idx: usize, metrics: Metrics) -> Result<()> {
     Ok(())
 }
 
-fn request_worker(metrics: Metrics) -> Result<()> {
+fn request_worker(metrics: AmapMetrics) -> Result<()> {
     thread::spawn(move || {
         loop {
             let mut rng = thread_rng();
---
+++ examples/cmetrics.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+use concurrency::CmapMetrics;
+use rand::{thread_rng, Rng};
+use std::{thread, time::Duration};
+
+const N: usize = 2;
+const M: usize = 4;
+
+fn main() -> Result<()> {
+    let metrics = CmapMetrics::new();
+
+    for idx in 0..N {
+        task_work(idx, metrics.clone())?; // Arc::clone
+    }
+
+    for _ in 0..M {
+        request_worker(metrics.clone())?;
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(2));
+        println!("{}", metrics);
+    }
+}
+
+fn task_work(idx: usize, metrics: CmapMetrics) -> Result<()> {
+    thread::spawn(move || {
+        let mut rng = thread_rng();
+        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+        metrics.inc(format!("call.thread.worker.{}", idx))?;
+        Ok::<_, anyhow::Error>(())
+    });
+    Ok(())
+}
+
+fn request_worker(metrics: CmapMetrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..5);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+    Ok(())
+}
